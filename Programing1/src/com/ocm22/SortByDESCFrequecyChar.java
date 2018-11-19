2a5b36ca5d5cf7aff06d0ce9b926204fd7decf88
user <user@Sonu>
2018-11-19 11:41:47 +0530

    first commit  uploading the files

---
+++ Programing1/src/com/ocm22/SortByDESCFrequecyChar.java
@@ -0,0 +1,42 @@
+package com.ocm22;
+import java.util.*;
+public class SortByDESCFrequecyChar 
+{
+	public static void main(String[] args) 
+	{
+      int [] array= {4,5,7,7,6,4,5,3};
+      Map<Integer, Integer> map= new HashMap<>();
+      List<Integer> outputArray= new ArrayList<>();
+      
+      for(int current:array) {
+    	  int count= map.getOrDefault(current, 0);
+    	  map.put(current, count + 1);
+    	  outputArray.add(current);
+      }
+      SortComparator comp=new SortComparator(map);
+      
+      Collections.sort(outputArray, comp);
+       for(Integer i: outputArray) {
+    	   System.out.print(i+" ");
+       
+       }
+	}
+
+}
+ class SortComparator implements Comparator<Integer>{
+	 private final Map<Integer,Integer> freqMap;
+	 SortComparator(Map<Integer, Integer>tFreqMap)
+	 {
+		 this.freqMap= tFreqMap;
+		 
+	 }
+	 public int compare(Integer k1,Integer k2)
+	 {
+		 int freqCompare= -freqMap.get(k2).compareTo(freqMap.get(k1));
+		 int valueCompare= k1.compareTo(k2);
+		 if(freqCompare==0)
+			 return valueCompare;
+		 else
+			 return freqCompare;
+	 }
+ }
